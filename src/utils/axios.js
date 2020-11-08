9c40a1fa79756accf0e487c8fe3529faf499ae14
anil-allipilli <anilkmr458@gmail.com>
2020-11-09 02:43:17 +0530

    https

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://127.0.0.1:8000/api/',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin' : '*',
